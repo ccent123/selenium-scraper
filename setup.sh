b27020797b70a7753028f3ddd7090a15f24abafb
Cristian Centeno <autonome@Cristians-MacBook-Pro.local>
2022-06-27 20:17:51 -0400

    feat(setup.sh): Added ability to clone git repo and auto install node packages

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 echo -ne '\n' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
 brew install node
+git clone "https://github.com/ccent123/selenium-scraper"
+cd ./selenium-scraper
 npm install
