9d8b9733d946fd874dd6e427190dd85937fee5ee
Cristian Centeno <autonome@Cristians-MacBook-Pro.local>
2022-06-27 20:22:58 -0400

    fix(setup): fixed inability to clone git repo

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 echo -ne '\n' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
 brew install node
-git clone "https://github.com/ccent123/selenium-scraper"
+git clone https://github.com/ccent123/selenium-scraper selenium-scraper
 cd ./selenium-scraper
 npm install
